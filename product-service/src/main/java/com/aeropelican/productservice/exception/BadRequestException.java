a008c2642a3846539d649f6b48c8ad643ab1eed1
feature/NithinReddy-calculator <knithinkumar30@gmail.com>
2026-08-14 11:50:11 +0530

    initial commit

--- product-service/src/main/java/com/aeropelican/productservice/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.aeropelican.productservice.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-
-}
--- product-service/src/main/java/com/aeropelican/productservice/exception/DuplicateResourceException.java
+++
@@ -1,8 +0,0 @@
-package com.aeropelican.productservice.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
